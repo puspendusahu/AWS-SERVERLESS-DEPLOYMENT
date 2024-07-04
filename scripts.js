f03f828367aabbd65e5f513ed29ab60f6b7a5816
puspendusahu <55124766+puspendusahu@users.noreply.github.com>
2024-07-04 08:53:03 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Add your API endpoint here
-var API_ENDPOINT = "API_ENDPOIND_PASTE_HERE";
+var API_ENDPOINT = "https://h8377jh9ac.execute-api.us-east-1.amazonaws.com/DEV";
 
 // AJAX POST request to save student data
 document.getElementById("savestudent").onclick = function(){
