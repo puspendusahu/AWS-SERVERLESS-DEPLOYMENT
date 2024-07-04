63560f56ec212dcd278dc85b3efd0cddcdf1c6b2
puspendusahu <55124766+puspendusahu@users.noreply.github.com>
2024-07-04 08:00:44 +0530

    Update insertStudentData.py

--- insertStudentData.py
+++ insertStudentData.py
@@ -4,12 +4,12 @@ import boto3
 # Create a DynamoDB object using the AWS SDK
 dynamodb = boto3.resource('dynamodb')
 # Use the DynamoDB object to select our table
-table = dynamodb.Table('studentData')
+table = dynamodb.Table('StudentData')
 
 # Define the handler function that the Lambda service will use as an entry point
 def lambda_handler(event, context):
     # Extract values from the event object we got from the Lambda service and store in variables
-    student_id = event['studentid']
+    student_id = event['studentId']
     name = event['name']
     student_class = event['class']
     age = event['age']
@@ -17,7 +17,7 @@ def lambda_handler(event, context):
     # Write student data to the DynamoDB table and save the response in a variable
     response = table.put_item(
         Item={
-            'studentid': student_id,
+            'studentId': student_id,
             'name': name,
             'class': student_class,
             'age': age
